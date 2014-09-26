16a2e4f5ba7fb38a7dc1de10e8e13af6b2883903
seankookookoo <tensi0704@hotmail.com>
2014-09-26 16:40:44 -0700

    Added arcade drive.

--- src/robot/Drive.java
+++ src/robot/Drive.java
@@ -1,16 +1,33 @@
 package robot;
 
 import robot.util.control.XboxController;
+import robot.util.drive.WoodchuckDrive;
 
 public class Drive implements RobotComponent {
 
     public XboxController controller;
+    public WoodchuckDrive drive;
     public void init() {
+        controller = new XboxController();
+        drive = new WoodchuckDrive();
+        
     }
 
     public void teleop() {
+        double leftStickX = controller.getAxis(XboxController.LEFT_X);
+        double leftStickY = controller.getAxis(XboxController.LEFT_Y);
+        double rightStickX = controller.getAxis(XboxController.RIGHT_X);
+        double rightStickY = controller.getAxis(XboxController.RIGHT_Y);
+        
+        //tankDrive
+        //drive.setLeft(leftStickY);
+        //drive.setRight(rightStickY);
+        
+        //arcadeDrive
+        drive.setLeft(leftStickY + rightStickX);
+        drive.setRight(leftStickY - rightStickX);
+    
     }
-
     public void auton() {
     }
 
